2049082db4321b2c05729cdf162b4c3a87275b94
lula1011 <lula1011@hs-karlsruhe.de>
2021-05-17 00:46:30 +0200

    Aufgabe2fertig

--- Aufgabe2/gta_v2/public/javascripts/geotagging.js
+++ Aufgabe2/gta_v2/public/javascripts/geotagging.js
@@ -12,12 +12,17 @@ console.log("The script is going to start...");
 
 // Hier wird die verwendete API für Geolocations gewählt
 // Die folgende Deklaration ist ein 'Mockup', das immer funktioniert und eine fixe Position liefert.
+
+Test = function(parameter) {
+    var i = 0;
+}
+
 GEOLOCATIONAPI = {
     getCurrentPosition: function(onsuccess) {
         onsuccess({
             "coords": {
-                "latitude": 49.013790,
-                "longitude": 8.390071,
+                "latitude": 40.730610,
+                "longitude": -73.935242,
                 "altitude": null,
                 "accuracy": 39,
                 "altitudeAccuracy": null,
@@ -46,11 +51,15 @@ var gtaLocator = (function GtaLocator(geoLocationApi) {
      * Bei Fehler Callback 'onerror' mit Meldung.
      * Callback Funktionen als Parameter übergeben.
      */
-    var tryLocate = function(onsuccess, onerror) {
-        if (geoLocationApi) {
-            geoLocationApi.getCurrentPosition(onsuccess, function(error) {
+    var tryLocate = function(onsuccess, onerror) 
+    {
+        if (geoLocationApi) 
+        {
+            geoLocationApi.getCurrentPosition(onsuccess, function(error) 
+            {
                 var msg;
-                switch (error.code) {
+                switch (error.code) 
+                {
                     case error.PERMISSION_DENIED:
                         msg = "User denied the request for Geolocation.";
                         break;
@@ -66,7 +75,9 @@ var gtaLocator = (function GtaLocator(geoLocationApi) {
                 }
                 onerror(msg);
             });
-        } else {
+        } 
+        else 
+        {
             onerror("Geolocation is not supported by this browser.");
         }
     };
@@ -82,7 +93,7 @@ var gtaLocator = (function GtaLocator(geoLocationApi) {
     };
 
     // Hier API Key eintragen
-    var apiKey = "YOUR_API_KEY_HERE";
+    var apiKey = "ExnUedkUKpTotPIXwNpcqEhrsIFpnNvs";
 
     /**
      * Funktion erzeugt eine URL, die auf die Karte verweist.
@@ -121,6 +132,27 @@ var gtaLocator = (function GtaLocator(geoLocationApi) {
 
         updateLocation: function() {
             // TODO Hier Inhalt der Funktion "update" ergänzen
+            //tryLocate(value coords ersetzen, alert)
+            var onsuccess = function(position)
+            {
+                var latitude = getLatitude(position);
+                var longitude = getLongitude(position);
+                document.getElementById("LAT").value = latitude;
+                document.getElementById("LONG").value = longitude;
+                document.getElementById("hiddenLAT").value = latitude;
+                document.getElementById("hiddenLONG").value = longitude;
+
+                var mapURL = getLocationMapSrc(latitude, longitude); //returns map-snippet URL
+                document.getElementById("result-img").src = mapURL;
+            };
+
+            var onerror = function(errormsg)
+            {
+                alert(errormsg);
+            };
+
+            tryLocate(onsuccess, onerror);
+
         }
 
     }; // ... Ende öffentlicher Teil
@@ -132,6 +164,7 @@ var gtaLocator = (function GtaLocator(geoLocationApi) {
  * des Skripts.
  */
 $(function() {
-    alert("Please change the script 'geotagging.js'");
+    alert("Your current position is set as default.");
     // TODO Hier den Aufruf für updateLocation einfügen
+    gtaLocator.updateLocation();
 });
